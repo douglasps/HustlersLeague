6ec2efbbb98b3a97a0592e206d488399394148a4
Ana Paula <Ana Paula>
2018-03-30 17:33:13 -0300

    little form corrections

--- src/js/views/index.js
+++ src/js/views/index.js
@@ -1,11 +1,11 @@
 document.querySelector("#formEmployer").addEventListener("click", function() {
-	document.querySelector("#forEmployee").classList.remove("show");
+	document.querySelector("#formEmployee").classList.remove("show");
 	console.log("fecha o candidato");
 });
 
 document.querySelector("#formEmployee").addEventListener("click", function() {
 	console.log("fecha o contratante");
-	document.querySelector("#forEmployer").classList.remove("show");
+	document.querySelector("#formEmployer").classList.remove("show");
 });
 
 (function(){

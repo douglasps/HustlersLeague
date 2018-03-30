2b2b7692429b1b0c11eca50cbef778c13145fc8a
Ana Paula <Ana Paula>
2018-03-30 18:07:38 -0300

    button EmployeeEmployer correction

--- src/js/views/index.js
+++ src/js/views/index.js
@@ -1,10 +1,8 @@
-document.querySelector("#formEmployer").addEventListener("click", function() {
+document.querySelector("#typeEmployer").addEventListener("click", function() {
 	document.querySelector("#formEmployee").classList.remove("show");
-	console.log("fecha o candidato");
 });
 
-document.querySelector("#formEmployee").addEventListener("click", function() {
-	console.log("fecha o contratante");
+document.querySelector("#typeEmployee").addEventListener("click", function() {
 	document.querySelector("#formEmployer").classList.remove("show");
 });
 
